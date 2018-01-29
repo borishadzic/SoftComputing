65d2ef976599b101938cae3dbe07c441563edeb5
Boris Hadzic <hadzicboris@gmail.com>
2018-01-29 01:42:12 +0100

    Initial commit

---
+++ application.py
@@ -0,0 +1,176 @@
+import numpy as np
+import cv2
+import math
+
+from lines import get_lines, draw_lines, Line
+from keras import models
+from vector import distance
+from trackednumber import TrackedNumber
+
+def findClosest(number, prevNumbers):
+    for prevNumber in prevNumbers:
+        dist = distance(number.get_bottom_right(), 
+                        prevNumber.get_bottom_right())
+
+        if dist < 20:
+            return prevNumber
+            
+    return None
+
+def update_tracked_numbers(img_bin, trackedNumbers):
+    """
+    Pronalazi konture sa slike, za svaku konturu pokusava da pronadje 
+    element sa prethodnog frejma koji odgovara novoj konturi. Ako je element
+    pronadjen njegova pozicija ce biti azurirana sa koordinatama nove konture.
+    U slucaju da prethodni element nije pronadjen, kontura ce biti sacuvana
+    kao novi element u listu
+    """
+    contours = cv2.findContours(img_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+    for contour in contours:
+        coords = cv2.boundingRect(contour)
+        
+        # ako je visina konture veca od 9
+        if coords[3] > 9:
+            number = TrackedNumber(coords)
+
+            prevNumber = findClosest(number, trackedNumbers)
+
+            if prevNumber is None:
+                # ako nije pronadjen prethnodni broj koji odgovara novoj konturi
+                # dodaj ga u listu
+                trackedNumbers.append(number)       
+            else:
+                # ako je pronadjen prethodni broj koji odgovara novoj konturi
+                # aziraraj njegovu poziciju novim kooordinatama
+                prevNumber.update_position(coords)
+
+
+def prepare_for_ann(img_bin, number):
+    """
+    Metoda koja sece deo slike sa binarne slike tako da obuhvati ceo broj.
+    Iseceni deo slike na kojoj se trazeni broj se kasnije pretvara u oblik
+    pogodan za predikciju u neuronskoj mrezi
+    """
+
+    # uzmi koordinate gornjeg levog i donjeg desnog coska konture
+    p1 = number.get_top_left()
+    p2 = number.get_bottom_right()
+
+    # sa binarne slike iseci deo slike koji odgovara tim koordinatama
+    # i jos 7 piksela sa svih strana slike
+    extra = 7
+    img_number = img_bin[p1[1] - extra : p2[1] + extra, 
+                         p1[0] - extra : p2[0] + extra]  
+    img_number = cv2.GaussianBlur(img_number, (5, 5), 0)
+
+    # pretvori ga u oblik pogodan za predikciju u neuronskoj mrezi
+    resized = cv2.resize(img_number, (28, 28), interpolation = cv2.INTER_NEAREST)
+    scale = resized / 255
+    mVector = scale.flatten()
+    mColumn = np.reshape(mVector, (1, 784))
+
+    return np.array(mColumn, dtype=np.float32)
+
+def winner(output):
+    return max(enumerate(output), key=lambda x: x[1])[0]
+
+def get_result_from_alphabet(outputs, alphabet):
+    result = []
+    for output in outputs:
+        result.append(alphabet[winner(output)])
+    return result
+
+def get_prediciton(model, img_number):
+    alphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    predicted_result = model.predict(img_number)
+    final_result = get_result_from_alphabet(predicted_result, alphabet)[0]
+
+    return final_result
+
+def display_prediction(frame, predicted_number, number):
+    """
+    Pomocna metoda koja na prodledjenoj slici iscrtava prediktovanu vrednost
+    """
+    p1 = number.get_top_left()
+    p2 = number.get_bottom_right()
+    cv2.rectangle(frame, p1, p2, (0,255,0), 2)
+    cv2.putText(frame, str(predicted_number), (p1[0], p1[1] - 3), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+def display_sum(frame, sum_blue, sum_green):
+    """
+    Pomocna metoda koja ispisuje trenutne sume. Plavom bojom je ispisana
+    suma brojeva koji su prosli kroz plavu liniju. Zelenom bojom je ispisana suma
+    brojeva koji su prosli kroz zelenu liniju
+    """
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, str(sum_blue), (10, 45), font, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, str(sum_green), (10, 75), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, 'Ukupno ' + str(sum_blue-sum_green), (10, 105), font,  0.8, (0, 255, 255), 1, cv2.LINE_AA)
+
+def main(model, video_src, canPause = False):
+    cap = cv2.VideoCapture(video_src)
+    
+    first_frame = cap.read()[1]
+    blue_line, green_line = get_lines(first_frame)
+
+    sum_blue, sum_green = 0, 0
+    trackedNumbers = []
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        img_bin = cv2.threshold(img_gray, 160, 255, cv2.THRESH_BINARY)[1]
+
+        update_tracked_numbers(img_bin, trackedNumbers)     
+        
+        pause = False
+
+        for number in trackedNumbers:
+            if not number.passedBlue():
+                if blue_line.has_passed(number):
+                    number.setPassedBlue(True)
+
+                    img_number = prepare_for_ann(img_bin, number)
+                    predicted_number = get_prediciton(model, img_number)
+                    display_prediction(frame, predicted_number, number)
+                    sum_blue += predicted_number
+
+                    pause = True
+
+            if not number.passedGreen():
+                if green_line.has_passed(number):
+                    number.setPassedGreen(True)
+
+                    img_number = prepare_for_ann(img_bin, number)
+                    predicted_number = get_prediciton(model, img_number)
+                    display_prediction(frame, predicted_number, number)
+                    sum_green += predicted_number
+
+                    pause = True
+
+        # display_sum(frame, sum_blue, sum_green)
+        # draw_lines(frame, [blue_line, green_line])
+
+        cv2.imshow(video_src, frame)
+
+        if pause and canPause:
+            pause = False
+            cv2.waitKey(0)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return sum_blue - sum_green
+
+if __name__ == '__main__':
+    model = models.load_model('model2.h5')
+    main(model, 'Video/video-0.avi', True)
---
+++ lines.py
@@ -0,0 +1,150 @@
+import cv2
+import numpy as np
+
+from scipy.spatial import distance
+from trackednumber import TrackedNumber
+
+class Line:
+    def __init__(self, line):
+        coords = line[0]
+        self.__p1 = (coords[0], coords[1])
+        self.__p2 = (coords[2], coords[3])
+  
+    def get_first_point(self):
+        """
+        Vraca koordinate prve (leve) tacke linije.
+        Primer (26, 78)
+        """
+        return self.__p1
+    
+    def get_second_point(self):
+        """
+        Vraca koordinate druge (desne) tacke linije.
+        Primer (60, 20)
+        """
+        return self.__p2
+
+    def has_passed(self, number : TrackedNumber):
+        """
+        Proverava udaljenost proslednjenog broja od samog broja.
+        Za udaljenost manju od 5 racuna se da je broj prosao liniju.
+        U obzir uzima samo brojeve koji se nalazi izmedju x koordinata
+        same linije.
+        """
+        dot = number.get_bottom_right()
+
+        if self.__p1[0] < dot[0] and dot[0] < self.__p2[0]:
+            p1 = np.array(self.__p1)
+            p2 = np.array(self.__p2)
+            p3 = np.array(dot)
+            dist = float(np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1))
+
+            return dist <= 7
+        else:
+            return False
+
+def get_lines(frame):
+    """
+    Glavna metoda koja poziva sve ostale. Vraca 
+    dve linije linije pronadjene hough transformacijom.
+    Prva linija je plava, druga je zelena
+    """
+    blue_output, green_output = separate_lines(frame)
+
+    blue_lines = find_all_lines(blue_output)
+    blue_line = longest_line(blue_lines)
+
+    green_lines = find_all_lines(green_output)
+    green_line = longest_line(green_lines)
+
+    return Line(blue_line), Line(green_line)
+
+def find_all_lines(img):
+    """
+    Metoda koja pomocu hough transformacije pronalazi sve prave linije koje 
+    se nalaze na slici
+    """
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_bin = cv2.threshold(img_gray, 10, 255, cv2.THRESH_BINARY)[1]
+
+    img_edges = cv2.Canny(img_bin, 100, 250)
+    img_blur = cv2.GaussianBlur(img_edges, (7, 7), 1)
+    lines = cv2.HoughLinesP(img_blur, 1,  np.pi / 180, 50, 50, 150)
+
+    return lines
+
+def longest_line(lines):
+    """
+    Pomocna funkcija koja pronalazi najduzu liniju iz prosledjenih linija
+    """
+    l_line = lines[0]
+
+    coords = l_line[0]
+    al = (coords[0], coords[1])
+    bl = (coords[2], coords[3])
+    m_dist = distance.euclidean(al,bl) 
+
+    for line in lines:
+        coords = line[0]
+        a = (coords[0], coords[1])
+        b = (coords[2], coords[3])
+        dst = distance.euclidean(a,b)
+
+        if dst >= m_dist:
+            m_dist = dst
+            l_line = line
+
+    return l_line
+
+def draw_lines(img, lines):
+    """
+    Pomocna funkcija koja iscrtava linije pronadjene hough transfromacijom nad
+    prosledjenom slikom
+    """
+    for line in lines:
+        cv2.line(img, line.get_first_point(), line.get_second_point(), (0, 0, 255), 1)
+
+def separate_lines(image):
+    """
+    Pomocna funkcija koja sa prosledjene slike izdvaja zelene i plave regione.
+    Povratna vrednost funkcije su dve slike. Prva slika sadrzi samo plavu liniju,
+    druga slika sadrzi samo zelenu liniju.
+
+    """
+    blueBoundaries = ([120, 0, 0], [255, 100, 100])  
+    greenBoundaries = ([0, 130, 0], [50, 255, 50])
+
+    lower = np.array(blueBoundaries[0], dtype = "uint8")
+    upper = np.array(blueBoundaries[1], dtype = "uint8")
+    mask = cv2.inRange(image, lower, upper)
+    blue_output = cv2.bitwise_and(image, image, mask = mask)
+
+    lower = np.array(greenBoundaries[0], dtype = "uint8")
+    upper = np.array(greenBoundaries[1], dtype = "uint8")
+    mask = cv2.inRange(image, lower, upper)
+    green_output = cv2.bitwise_and(image, image, mask = mask)
+
+    return blue_output, green_output
+
+def main():
+    cap = cv2.VideoCapture('Video/video-9.avi')
+    ret, frame = cap.read()
+
+    if ret:
+        blue_output, green_output = separate_lines(frame)
+        blue_line, green_line = get_lines(frame)
+
+        draw_lines(blue_output, [blue_line])
+        draw_lines(green_output, [green_line])
+
+        cv2.imshow('Separated Lines', np.concatenate((blue_output, green_output), axis=1))
+        cv2.waitKey(0)
+
+    cap.release()
+    cv2.destroyAllWindows()   
+
+if __name__ == '__main__':
+    main()
+
+
+
